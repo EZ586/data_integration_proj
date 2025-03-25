c100154f88bc817772704951721f5a08c7d4b354
Zou, James <jameszou908@gmail.com>
2025-03-25 14:52:43 -0400

    Average aqi

---
+++ python_scripts/machine_learning_techniques.py
@@ -0,0 +1,7 @@
+"""
+Snippet from total_aqi_nccs_data.csv:
+CITY,CBSA,Percent_Unhealthy_Days,Latitude,Longitude,STATE,Total_Rev_Prog_Desc
+ABERDEEN,"Aberdeen, SD",0.0,45.4649805,-98.487813,SD,5879135.0
+ADJUNTAS,"Adjuntas, PR",0.0,18.18130935,-66.7524515474532,,
+ADRIAN,"Adrian, MI",0.0,41.8975152,-84.0373054,MI,1352099.0
+"""
